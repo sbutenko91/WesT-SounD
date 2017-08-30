4abbd872f7e803d9a7d7afcddc3148cec904eb04
Fullstack Developer <dedicatedbusinessunit@gmail.com>
2017-08-30 15:49:36 +0300

    completed all sections content

---
+++ public/js/content.js
@@ -0,0 +1,13 @@
+var show = false;
+
+function showModal() {
+    document.getElementById("imageModal").style.display = 'table';
+}
+
+function closeModal() {
+    document.getElementById("imageModal").style.display = 'none';
+}
+
+document.getElementsByClassName("lightwidget__image-wrapper").onclick = function showModal() {
+    
+}
--- public/js/header.js
+++ public/js/header.js
@@ -3,17 +3,44 @@ var isCollapsed = false;
 function toggleHeaderNavBar() {
     isCollapsed = !isCollapsed;
 
-    if (isCollapsed || scrollPosition > 300) {
-        document.getElementById("navbar").classList.add('show');
-        document.getElementById("navbarSupportedContent").classList.add('show');
-        document.getElementById("menu").classList.add('menuMobile');
+    if (isCollapsed) {
+        $("#navbar").addClass('show');
+        $("#navbarSupportedContent").removeClass('show');
+        $("#menu").addClass('menuMobile');
 
     } else {
-        document.getElementById("navbar").classList.remove('show');
-        document.getElementById("navbarSupportedContent").classList.remove('show');
-        document.getElementById("menu").classList.remove('menuMobile');
+        $("#navbar").removeClass('show');
+        $("#navbarSupportedContent").addClass('show');
+        $("#menu").removeClass('menuMobile');
     }
 }
+
+$(document).ready(function(){
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 400) {
+			$('.scrollToTop').css('display', 'block');
+		} else {
+			$('.scrollToTop').css('display', 'none');
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+	
+});
+
+$(document).ready(function(){
+    $("#navbar").on("click","a.nav-link", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top-98;
+        $('body,html').animate({scrollTop: top}, 800);
+    });
+});
 
 $(document).ready(function () {
     var scroll_start = 0;
@@ -29,43 +56,73 @@ $(document).ready(function () {
             }
         });
     }
-});
-
-var activeIndex = 100;
+});  
 
 function selectMenuItem(index) {
-    activeIndex = index
-    isCollapsed = false
-    console.log(activeIndex);
-    if (activeIndex == 1) {
+    if (index == 1) {
         document.getElementById("menu1").classList.add('active');
     } else {
         document.getElementById("menu1").classList.remove('active');
     }
-
-    if (activeIndex == 2) {
+    if (index == 2) {
         document.getElementById("menu2").classList.add('active');
     } else {
         document.getElementById("menu2").classList.remove('active');
     }
-    if (activeIndex == 3) {
+    if (index == 3) {
         document.getElementById("menu3").classList.add('active');
     } else {
         document.getElementById("menu3").classList.remove('active');
     }
-    if (activeIndex == 4) {
+    if (index == 4) {
         document.getElementById("menu4").classList.add('active');
     } else {
         document.getElementById("menu4").classList.remove('active');
     }
-    if (activeIndex == 5) {
+    if (index == 5) {
         document.getElementById("menu5").classList.add('active');
     } else {
         document.getElementById("menu5").classList.remove('active');
     }
-    if (activeIndex == 6) {
+    if (index == 6) {
         document.getElementById("menu6").classList.add('active');
     } else {
         document.getElementById("menu6").classList.remove('active');
     }
 }
+
+
+$(document).ready(function () {
+    $(document).on("scroll", () => {
+        var scrollPos = $(document).scrollTop();
+        scrollPos += 100;
+        var top = document.getElementById('top')
+        var service = document.getElementById('service')
+        var aboutus = document.getElementById('aboutus')
+        var equipment = document.getElementById('equipment')
+        var gallery = document.getElementById('gallery')
+        var clients = document.getElementById('clients')
+        var contact = document.getElementById('contact')
+        if (scrollPos > top.offsetTop) {
+            selectMenuItem(100);
+        }
+        if (scrollPos > service.offsetTop) {
+            selectMenuItem(1);
+        }
+        if (scrollPos > aboutus.offsetTop) {
+            selectMenuItem(2);
+        }
+        if (scrollPos > equipment.offsetTop) {
+             selectMenuItem(3);
+        }
+        if (scrollPos > gallery.offsetTop) {
+             selectMenuItem(4);
+        }
+        if (scrollPos > clients.offsetTop) {
+              selectMenuItem(5);
+        }
+        if (scrollPos > contact.offsetTop) {
+             selectMenuItem(6);
+        }
+    });
+})
